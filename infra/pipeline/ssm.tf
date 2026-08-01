8ff98e952cf8328d76a5eb07868bc998cde7bbc9
David Shubov <david.shubov@gmail.com>
2026-07-31 20:15:30 -0500

    feat: kinesis stream name ssm param

--- infra/pipeline/ssm.tf
+++ infra/pipeline/ssm.tf
@@ -1,3 +1,10 @@
 data "aws_ssm_parameter" "data_bucket_name" {
   name = "/relic/data/bucket_name"
 }
+
+resource "aws_ssm_parameter" "kinesis_stream_name" {
+  name        = "/relic/pipeline/kinesis_stream_name"
+  description = "Name of the kinesis stream"
+  type        = "String"
+  value       = aws_kinesis_stream.relic_stream.name
+}
